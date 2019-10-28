d2bc28158177e4d2af955e49d03b0dfc43d42323
Octavi Fornés <ofornes@base.cat>
2019-10-28 08:35:36 +0100

    Inicial

---
+++ src/main/java/cat/fornes/conceptes/nosql/NosqlTagsApplication.java
@@ -0,0 +1,13 @@
+package cat.fornes.conceptes.nosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosqlTagsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NosqlTagsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/fornes/conceptes/nosql/NosqlTagsApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.fornes.conceptes.nosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NosqlTagsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
